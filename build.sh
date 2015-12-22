2913922e23985ce723dd70d707a0fee830d7a2cf
Ubuntu <ubuntu@ip-172-31-20-26.eu-west-1.compute.internal>
2015-12-22 16:56:21 +0000

    blabla

--- build.sh
+++ build.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-PORT=8000
+PORT=80
 INSTANCE=my_nginx
 IMAGE=procrastinatio/varnish-config-test
 url=http://localhost:${PORT}
 
-
-DIR=$HOME/droneio
-
-
 TAG=$(date +%s)
-
-sudo docker kill ${INSTANCE}  && sudo docker rm ${INSTANCE}
-
-
 
 sudo docker build -t ${IMAGE}:${TAG} .
 
-
 sudo docker run --name ${INSTANCE}  -p ${PORT}:80  -d ${IMAGE}:${TAG}
 
-status=$(curl --write-out %{http_code} --silent --output /dev/null "${url}/")
-
-echo ${status}
-
-[ ${status} -eq 200 ]   && sudo docker push ${IMAGE}:${TAG}  && sudo docker rm  ${IMAGE}:${TAG}
+nose2
+[ $? -eq  0 ]   && sudo docker push ${IMAGE}:${TAG}  && sudo docker kill ${INSTANCE}  && sudo docker rm ${INSTANCE}
 
